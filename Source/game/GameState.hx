c0199b0256aea7cee10cf9d67f0895482368435e
MintPaw <jerusanders@gmail.com>
2015-04-03 23:04:14 -0600

    Rendered 3 layers of tilemap

--- Source/game/GameState.hx
+++ Source/game/GameState.hx
@@ -13,7 +13,7 @@ class GameState extends FlxState
 
 	private var _players:Array<Player>;
 	private var _playerDefs:Array<PlayerDef>;
-	private var _tilemap:FlxTilemap;
+	private var _tilemaps:Array<FlxTilemap>;
 
 	private var _mapName:String;
 
@@ -28,23 +28,38 @@ class GameState extends FlxState
 	override public function create():Void
 	{
 		{ // Create Tilemap
-			var tilemapString:String = Assets.getText("Assets/map/" + _mapName + ".tmx");
-			tilemapString = tilemapString.split("<data encoding=\"csv\">")[1];
-			tilemapString = tilemapString.split("</data>")[0];
-			trace(tilemapString);
+			_tilemaps = [];
 
-			_tilemap = new FlxTilemap();
-			_tilemap.loadMapFromCSV(tilemapString, Assets.getBitmapData("Assets/img/tilemap.png"), 32, 32, null, 1);
-			add(_tilemap);
+			var all:String = Assets.getText("Assets/map/" + _mapName + ".tmx");
+
+			var bot:String = all.split("<data encoding=\"csv\">")[1];
+			bot = bot.split("</data>")[0];
+
+			var mid:String = all.split("<data encoding=\"csv\">")[2];
+			mid = mid.split("</data>")[0];
+
+			var top:String = all.split("<data encoding=\"csv\">")[3];
+			top = top.split("</data>")[0];
+
+			_tilemaps.push(new FlxTilemap());
+			_tilemaps.push(new FlxTilemap());
+			_tilemaps.push(new FlxTilemap());
+
+			_tilemaps[0].loadMapFromCSV(bot, Assets.getBitmapData("Assets/img/tilemap.png"), 32, 32, null, 1);
+			_tilemaps[1].loadMapFromCSV(mid, Assets.getBitmapData("Assets/img/tilemap.png"), 32, 32, null, 1);
+			_tilemaps[2].loadMapFromCSV(top, Assets.getBitmapData("Assets/img/tilemap.png"), 32, 32, null, 1);
+
+			for (i in 0...3) add(_tilemaps[i]);
 		}
 
 		{ // Create players
 			_players = [];
 
-			for (i in _playerDefs)
+			for (i in 0..._playerDefs.length)
 			{
 				var p:Player = new Player();
-				//p.x = 
+				//p.x = _tilemap.widthInTiles / 2 - 2 + i;
+				//p.y =
 			}
 		}
 	}
